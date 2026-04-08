a2ae3d5e7b476213a549c89922ae8cbcdbd446db
Myles Loffler <myles2007@gmail.com>
2026-04-08 09:07:06 -0500

    feat(nvim): gitsigns blame window border, high contrast splits

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,7 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.api.nvim_set_hl(0, "VertSplit", { bg = "#FFFFFF" })
+vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#0CFF15" })
+vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#0CFF15", bg = "NONE" })
+
 -- vim.diagnostic.config({
 --   virtual_text = false, -- Disables inline diagnostic text
 --   virtual_lines = true, -- Uses line-based diagnostic text for a bit more room...
---
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,8 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  opts = {
+    preview_config = {
+      border = { "╔", "═", "╗", "║", "╝", "═", "╚", "║" },
+    },
+  },
+}
